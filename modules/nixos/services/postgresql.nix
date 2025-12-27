91f02a4d41d7426d1631cf036201feb95cc32215
Diogo <hello@diogocastro.net>
2025-12-27 17:03:38 +0000

    services/postgresql: accept all hosts on 100.100.0.0/16

--- modules/nixos/services/postgresql.nix
+++ modules/nixos/services/postgresql.nix
@@ -42,7 +42,7 @@ in
         host all all 127.0.0.1/32 md5
         host all all ::1/128 md5
 
-        host all all 100.100.10.0/24 md5
+        host all all 100.100.0.0/16 md5
       '';
 
       # https://pgconfigurator.cybertec.at/
